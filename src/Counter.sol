26316db9d8053b27c794cc352f5543d6cdb8a60a
v-for-vasya <11951513+v-for-vasya@users.noreply.github.com>
2024-03-01 23:35:57 -0800

    Update Counter.sol

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,16 +2,20 @@
 pragma solidity ^0.8.24;
 
 import {BaseHook} from "v4-periphery/BaseHook.sol";
-
+import {FixedPointMathLib} from "solady/utils/FixedPointMathLib.sol"; 
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {Currency, CurrencyLibrary} from "v4-core/src/types/Currency.sol";
 
 contract Counter is BaseHook {
     using PoolIdLibrary for PoolKey;
-
+    using FixedPointMathLib for uint256;
+    using FixedPointMathLib for int256;
+    using CurrencyLibrary for Currency;
+    
     // NOTE: ---------------------------------------------------------
     // state variables should typically be unique to a pool
     // a single hook contract should be able to service multiple pools
@@ -45,14 +49,55 @@ contract Counter is BaseHook {
     // -----------------------------------------------
     // NOTE: see IHooks.sol for function documentation
     // -----------------------------------------------
+        function getTokenInAmount(IPoolManager.SwapParams calldata params) public pure returns (uint256) {
 
-    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
+        return 7e18;
+    }
+
+    /// @notice Calculate the amount of tokens sent to the swapper
+    /// @param params SwapParams passed to the swap function
+    /// @return The amount of tokens sent to the swapper
+    function getTokenOutAmount(IPoolManager.SwapParams calldata params, Currency inputToken, Currency outputToken) public returns (uint256) {
+
+        uint256 inputAmount; 
+        uint256 inputReserves = inputToken.balanceOfSelf();
+        uint256 outputReserves = outputToken.balanceOfSelf();
+        
+        int256 dx=int256(inputAmount);
+        int256 y=int256(outputReserves);
+        int256 x=int256(inputReserves);
+
+        // we didn't add 
+        int256 top = FixedPointMathLib.expWad(2-(x-dx));
+        int256 bottom = x-dx;
+        int256 frac=FixedPointMathLib.rawSDiv(top,bottom);
+        int256 LAMMbert=-FixedPointMathLib.lambertW0Wad(frac)-y;
+        return uint256(LAMMbert);
+    }
+
+    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
         external
         override
         returns (bytes4)
     {
-        beforeSwapCount[key.toId()]++;
-        return BaseHook.beforeSwap.selector;
+                // determine inbound/outbound token based on 0->1 or 1->0 swap
+        (Currency inbound, Currency outbound) =
+            params.zeroForOne ? (key.currency0, key.currency1) : (key.currency1, key.currency0);
+
+        // calculate the amount of tokens, based on a custom curve
+        uint256 tokenInAmount = getTokenInAmount(params); // amount of tokens paid by the swapper
+        uint256 tokenOutAmount = getTokenOutAmount(params, inbound, outbound); // amount of tokens sent to the swapper
+
+
+        // inbound token is added to hook's reserves, debt paid by the swapper
+        poolManager.take(inbound, address(this), tokenInAmount);
+
+        // outbound token is removed from hook's reserves, and sent to the swapper
+        outbound.transfer(address(poolManager), tokenOutAmount);
+        poolManager.settle(outbound);
+
+        // prevent normal v4 swap logic from executing
+        return Hooks.NO_OP_SELECTOR;
     }
 
     function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
